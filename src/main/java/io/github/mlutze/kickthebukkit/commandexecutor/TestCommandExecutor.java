988974c28e3110119a1da7a72a89caa63ba4f51d
Matt Lutze <mcdlutze@gmail.com>
2019-04-20 11:32:31 -0700

    add sample command and test

---
+++ src/main/java/io/github/mlutze/kickthebukkit/commandexecutor/TestCommandExecutor.java
@@ -0,0 +1,13 @@
+package io.github.mlutze.kickthebukkit.commandexecutor;
+
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandExecutor;
+import org.bukkit.command.CommandSender;
+
+public class TestCommandExecutor implements CommandExecutor {
+    @Override
+    public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
+        sender.sendMessage("Test successful.");
+        return true;
+    }
+}
---
+++ src/test/java/io/github/mlutze/kickthebukkit/commandexecutor/TestCommandExecutorTest.java
@@ -0,0 +1,33 @@
+package io.github.mlutze.kickthebukkit.commandexecutor;
+
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandSender;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.Mockito;
+import org.mockito.junit.MockitoJUnitRunner;
+
+@RunWith(MockitoJUnitRunner.class)
+public class TestCommandExecutorTest {
+
+
+    private TestCommandExecutor sut;
+
+    @Before
+    public void setUp() {
+        sut = new TestCommandExecutor();
+    }
+
+    @Test
+    public void shouldSendMessageToPlayer() {
+        CommandSender commandSender = Mockito.mock(CommandSender.class);
+        Command command = Mockito.mock(Command.class);
+
+        Mockito.doNothing().when(commandSender).sendMessage("Test successful.");
+        sut.onCommand(commandSender, command, "label", new String[]{});
+
+        Mockito.verify(commandSender).sendMessage("Test successful.");
+    }
+
+}
